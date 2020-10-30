c98709db5af31c4cf1fc2063f85a42ff913f9818
Zihao Xu <zxql2015@mymail.pomona.edu>
2020-10-29 22:11:15 -0400

    Sentiment analysis

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # cs171_tweet_it_out
+
+## To create a virtual environment
+1. Run `python3 -m pip install --user virtualenv` to install virtualenv 
+2. Run `python3 -m venv <env_name>` to create a virtual environment using your own `env_name`
+3. Install all dependencies by running `pip install -r requirements.txt`
+
---
+++ scripts/0_data_processing/01_sentiment_scores.py
@@ -0,0 +1,39 @@
+""" A script that takes in the raw covid19_tweets data and 
+    calcualtes the sentiment label and score for each tweet
+"""
+import pandas as pd
+from pathlib import Path
+from tqdm import tqdm
+from transformers import pipeline
+
+# Constants
+REPO_ROOT = Path.cwd()
+
+
+# Helper function
+def get_sentiment(text):
+    res = classifier(text)[0]
+    return res['label'], res['score']
+
+
+if __name__ == '__main__':
+    # Read in the data
+    df_path = REPO_ROOT / 'data' / 'covid19_tweets.csv'
+    df = pd.read_csv(df_path)
+    tweets = df['text'].values
+
+    # Use hugging face for sentiment scoring
+    # nlptown/bert-base-multilingual-uncased-sentiment
+    classifier = pipeline('sentiment-analysis')
+    sentiment = []
+    sentiment_score = []
+
+    for idx in tqdm(range(len(tweets))):
+        tweet = tweets[idx]
+        label, score = get_sentiment(tweet)
+        sentiment.append(label)
+        sentiment_score.append(score)
+
+    df['sentiment'] = sentiment
+    df['sentiment_score'] = sentiment_score
+    df.to_csv(REPO_ROOT / 'data' / 'covid19_tweets_sentiment.csv', index=None)
