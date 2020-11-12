538fb3cb9f420339157d735faf41dc28ebb371a1
Zihao Xu <zxql2015@mymail.pomona.edu>
2020-11-12 10:25:59 -0500

    More data processing

--- scripts/0_data_processing/03_combine_clean_tweet_data.py
+++ scripts/0_data_processing/03_combine_clean_tweet_data.py
@@ -2,20 +2,19 @@ import pandas as pd
 import numpy as np
 from tweet import config
 import datetime
+import os
 
 
 if __name__ == '__main__':
     # Read in all subset data
-    df1 = pd.read_csv(config.data / 'covid19_tweets_id_subset.csv', header = None)
-    df2 = pd.read_csv(config.data / 'covid19_tweets_id_subset2.csv', header = None)
-    df3 = pd.read_csv(config.data / 'covid19_tweets_id_subset3.csv', header = None)
-    df4 = pd.read_csv(config.data / 'covid19_tweets_id_subset4.csv', header = None)
-    df5 = pd.read_csv(config.data / 'covid19_tweets_id_subset5.csv', header = None)
-    df6 = pd.read_csv(config.data / 'covid19_tweets_id_subset6.csv', header = None)
+    dfs = []
+    for f in os.listdir(config.data):
+        if 'subset' in f:
+            dfs.append(pd.read_csv(config.data / f, header = None))
 
     columns = ['user_name','user_location', 'user_description', 'user_created', 'user_followers', 'user_friends',
                'user_favourites', 'user_verified', 'date', 'full_text', 'hashtags', 'source', 'lang']
-    df = pd.concat([df1, df2, df3, df4, df5, df6], axis=0)
+    df = pd.concat(dfs, axis=0)
     df.columns = columns
     df = df.sort_values('date')
 
@@ -51,6 +50,9 @@ if __name__ == '__main__':
     # 4. drop duplicated rows
     df = df.drop_duplicates(subset=['full_text','date','user_name'])
     print("Shape of df after cleaning:", df.shape)
+    
+    # 5. add id column
+    df['id'] = list(range(len(df)))
 
     # Save to disk
     df.to_csv(config.data / 'covid19_tweets_clean.csv', index = None)
